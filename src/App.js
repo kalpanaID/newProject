3da81048bf133e907d940b7a58125283fad7faf0
kalpanaID <Kalpana.1@insurancedekho.com>
2021-10-27 01:48:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login  from './components/login/login';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Login></Login>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/login/login.js
@@ -0,0 +1,19 @@
+
+import '@coreui/coreui/dist/css/coreui.min.css'
+
+
+const Login = () => {
+    return (
+        <div class="card text-center">
+        <div class="card-header">Featured</div>
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p><a class="btn btn-primary" href="#">Go somewhere</a>
+        </div>
+        <div class="card-footer text-medium-emphasis">2 days ago</div>
+        </div>
+    );
+}
+
+
+export default Login;
