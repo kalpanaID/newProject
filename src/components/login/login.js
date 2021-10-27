b754fef8937871b57c8ba03e740df1b86ac78206
kalpanaID <Kalpana.1@insurancedekho.com>
2021-10-27 16:14:20 +0530

    login ui done

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,13 +4,22 @@ import '@coreui/coreui/dist/css/coreui.min.css'
 
 const Login = () => {
     return (
-        <div class="card text-center">
-        <div class="card-header">Featured</div>
+        <div class="col-sm-6" > 
+        <div class="card">
         <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p><a class="btn btn-primary" href="#">Go somewhere</a>
+            <h3>Login</h3>
+            <div>Sign into your account</div>
+            <div className='div'></div>
+            <select class="form-select" aria-label="Default select example" >
+                <option selected>Select User</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+            <div className='div'></div>
+            <button class="btn btn-secondary" type="button">Login</button>
         </div>
-        <div class="card-footer text-medium-emphasis">2 days ago</div>
+        </div>
         </div>
     );
 }
